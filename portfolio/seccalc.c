4bf2ee0ce8478cb85885cb4a57fff3c91ebd94ca
kevin-dough <54718041+kevin-dough@users.noreply.github.com>
2019-09-13 08:44:07 -0700

    final seconds calc updated

--- portfolio/seccalc.c
+++ portfolio/seccalc.c
@@ -11,15 +11,45 @@
 int seccalc(void) {
     
     int inputseconds;
-    printf("Enter an amount of seconds: \n");
-    scanf("%d", &inputseconds);
-    
-    printf("In %d seconds, there are:\n", inputseconds);
-    
     double days;
     double minutes;
     double hours;
     double seconds;
+    printf("Enter an amount of seconds: \n");
+    scanf("%d", &inputseconds);
+    
+    if (inputseconds==1) {
+        printf("In 1 second, there is 1 second. Why are you even asking that?\n");
+    }else if(inputseconds==0) {
+        printf("In 0 seconds, there are 0 seconds. Duh.");
+    }else if(inputseconds<0) {
+        printf("Why are you using negative numbers?");
+    } else {
+        printf("In %d seconds, there are:\n", inputseconds);
+        if(inputseconds>=86400) {
+            days = inputseconds/86400 -((inputseconds%86400)/86400);
+        } else {
+            days = 0;
+        }
+        
+        if((inputseconds%86400)>=3600) {
+            hours = (inputseconds%86400)/3600 - (((inputseconds%86400)%3600)/3600);
+        } else {
+            hours = 0;
+        }
+        
+        if(((inputseconds%86400)%3600)>=60) {
+            minutes = ((inputseconds%86400)%3600)/60 - ((((inputseconds%86400)%3600)%60)/60);
+        } else {
+            minutes = 0;
+        }
+        seconds = (((inputseconds%86400)%3600)%60);
+        
+        printf("Days:       %lf \n", days);
+        printf("Hours:      %lf \n", hours);
+        printf("Minutes:    %lf \n", minutes);
+        printf("Seconds:    %lf \n", seconds);
+    }
     
     /*
     days = (inputseconds/86400);
@@ -30,30 +60,6 @@ int seccalc(void) {
     
     seconds = (inputseconds -(86400*days)-(hours*24)-(minutes*60));
     */
-    
-    if(inputseconds>=86400) {
-        days = inputseconds/86400 -((inputseconds%86400)/86400);
-    } else {
-        days = 0;
-    }
-    
-    if((inputseconds%86400)>=3600) {
-        hours = (inputseconds%86400)/3600 - (((inputseconds%86400)%3600)/3600);
-    } else {
-        hours = 0;
-    }
-    
-    if(((inputseconds%86400)%3600)>=60) {
-        minutes = ((inputseconds%86400)%3600)/60 - ((((inputseconds%86400)%3600)%60)/60);
-    } else {
-        minutes = 0;
-    }
-    seconds = (((inputseconds%86400)%3600)%60)/60;
-    
-    printf("Days:       %lf \n", days);
-    printf("Hours:      %lf \n", hours);
-    printf("Minutes:    %lf \n", minutes);
-    printf("Seconds:    %lf \n", seconds);
     
     return 0;
 }
