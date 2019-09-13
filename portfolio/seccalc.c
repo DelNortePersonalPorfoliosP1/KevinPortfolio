07d8f1780d7831ab5b1c13843ace18bacf327b91
kevin-dough <54718041+kevin-dough@users.noreply.github.com>
2019-09-13 08:34:44 -0700

    seconds calculator update

--- portfolio/seccalc.c
+++ portfolio/seccalc.c
@@ -16,17 +16,44 @@ int seccalc(void) {
     
     printf("In %d seconds, there are:\n", inputseconds);
     
-    hours = (inputseconds/3600);
+    double days;
+    double minutes;
+    double hours;
+    double seconds;
     
-    minutes = (inputseconds -(3600*hours))/60;
+    /*
+    days = (inputseconds/86400);
     
-    seconds = (inputseconds -(3600*hours)-(minutes*60));
+    hours = (inputseconds -(86400*days))/24;
     
+    minutes = (inputseconds -(86400*days)-(hours*24));
     
-    printf("Days:       %lf \n", day);
-    //printf("Hours:      %lf \n", hour);
-    //printf("Minutes:    %lf \n", );
-    //printf("Hours:      %lf \n", );
+    seconds = (inputseconds -(86400*days)-(hours*24)-(minutes*60));
+    */
+    
+    if(inputseconds>=86400) {
+        days = inputseconds/86400 -((inputseconds%86400)/86400);
+    } else {
+        days = 0;
+    }
+    
+    if((inputseconds%86400)>=3600) {
+        hours = (inputseconds%86400)/3600 - (((inputseconds%86400)%3600)/3600);
+    } else {
+        hours = 0;
+    }
+    
+    if(((inputseconds%86400)%3600)>=60) {
+        minutes = ((inputseconds%86400)%3600)/60 - ((((inputseconds%86400)%3600)%60)/60);
+    } else {
+        minutes = 0;
+    }
+    seconds = (((inputseconds%86400)%3600)%60)/60;
+    
+    printf("Days:       %lf \n", days);
+    printf("Hours:      %lf \n", hours);
+    printf("Minutes:    %lf \n", minutes);
+    printf("Seconds:    %lf \n", seconds);
     
     return 0;
 }
